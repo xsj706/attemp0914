87677fd1e6e57dfc610878968da5e6c8e42b68db
Car Shaker <xsj706@gmail.com>
2014-09-14 12:49:32 -0700

    initial commit

---
+++ attemp0914/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Attemp0914::Application.config.secret_token = '506bc9e8f60c03c5e2d66e8792f27c26a291ffb12490bf665cce68af7e2ff6e46a5893dc12b7a0332d505970ff44a04e17f5247a9fdc18a3e198a9bebcdbdeb7'
